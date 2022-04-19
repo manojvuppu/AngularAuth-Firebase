14c68672b302aebc2e52178f5c475c9a128c50fb
manojvuppu <manoj.vuppu@gmail.com>
2022-04-19 10:01:19 -0400

    Adding USERS API to get permission denied issue

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,15 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
 })
 export class RecipesComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.getUsers();
   }
 
+  private getUsers() {
+    this.http
+      .get(
+        'https://ng-complete-guide-2657e-default-rtdb.firebaseio.com/users.json'
+      )
+      .pipe(
+        map((users) => {
+          const usersArray = [];
+          for (const key in users) {
+            if (users.hasOwnProperty(key)) {
+              usersArray.push({ ...users[key], id: key });
+            }
+          }
+          return usersArray;
+        })
+      )
+      .subscribe((resp) => console.log(resp));
+  }
 }
