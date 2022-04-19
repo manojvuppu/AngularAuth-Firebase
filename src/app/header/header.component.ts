a2b2b665a9a7656d13fb85075dc3acd401007833
manojvuppu <manoj.vuppu@gmail.com>
2022-04-19 11:48:19 -0400

    Header listening login status bug fix fx

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
 
@@ -7,15 +9,25 @@ import { AuthService } from '../auth/auth.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy{
   isLoggedIn: boolean = false;
+  private userSub: Subscription;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
-    this.authService.user.pipe(take(1)).subscribe((user) => {
+    this.userSub = this.authService.user.subscribe((user) => {
       this.isLoggedIn = !!user;
       console.log(this.isLoggedIn);
     });
+  }
+
+  logout() {
+    this.authService.user.next(null);
+    this.router.navigate(['/login']);
+  }
+
+  ngOnDestroy(){
+    this.userSub.unsubscribe();
   }
 }
